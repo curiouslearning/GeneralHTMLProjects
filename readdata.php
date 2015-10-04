dc2684f4156848499240250a42ef9a930e37d0bc
jasonkrone <jasonkrone@me.com>
2015-10-03 22:27:51 -0400

    formmating queries

--- readdata.php
+++ readdata.php
@@ -1,11 +1,13 @@
 <?php
-
+var_dump($_POST);
+/*
 ?>
 <div class="alert alert-success fade in" id="success_alert">
     <a href="#" class="close" data-dismiss="alert">&times;</a>
     <strong>Success!</strong> Your form has been sent successfully.
 </div>
 <?php
+*/
 
 function main() {
     $idName = "id";
@@ -19,8 +21,8 @@ function main() {
     $blendingName = "blending";
     $nonwordRepName = "nonword_rep";
     $commentsName = "comments";
-
-    $rows = 1;
+    $rowsName = "table_rows";
+    $rows = $_POST["$rowsName"];
 
     // collect ids
     $idArray = collectInputs($rows, $idName);
@@ -41,7 +43,7 @@ function main() {
 
     // receptive vocab score
     $recepVocabScoreArray = collectInputs($rows, $receptiveVocabName);
-    validate("is_numeric", $nonwordRepScoreArray);
+    validate("is_numeric", $recepVocabScoreArray);
 
     // letter id score
     $letterIDScoreArray = collectInputs($rows, $letterIDName);
@@ -64,8 +66,62 @@ function main() {
     validate("isDate", $scoringDateArray);
 
     // comments dont need to be validated TODO: prevent SQL injection
-    $comentArray = collectInputs($rows, $commentsName);
+    $commentArray = collectInputs($rows, $commentsName);
+/*
+    $names = array("id", "birth_date", "testing_date", "scoring_date", "receptive_vocab", "letter_id",
+                   "decodable_sight_words", "rhyming", "blending", "nonword_repition", "comments");
+
+    for ($i = 0; $i < $rows; $i++) {
+        // insert data into table
+        $data = array($idArray[$i], $rhymingScoreArray[$i], $blendingScoreArray[$i], $nonwordRepScoreArray[$i],
+                     $recepVocabScoreArray[$i], $letterIDScoreArray[$i], $decSightWordScoreArray[$i],
+                     $birthdayArray[$i], $testingDateArray[$i], $scoringDateArray[$i], $commentArray[$i]);
+        insertAssessmentData($data, $names);
+    }
+*/
 }
+
+/*
+// values
+// TODO: make this take types into account
+function sql_values($vals) {
+    $str = "(";
+    $i = 0;
+    $count = array_count_values($vals);
+    for ($names as $n) {
+        // form the $n
+        if (isDate($n)) {
+            // put it into the correct date format
+            $newDate = date("Y-d-m", strtotime($n));
+        } else if (is_string())
+            // put escaped quotes around it
+        }
+
+        $fields = $fields . $n;
+        if ( $i != $count - 1) {
+            $fields = $fields . ", ";
+        }
+        $i = $i + 1;
+    }
+    $fields = $fields . ")";
+    return $fields;
+}
+
+
+
+// inserts the given data into the assessment_data table under the given field names
+function insertAssessmentData($data, $names) {
+    // form the first part of the query
+    $names_str = array_reduce($names, "concat");
+    $sql = "INSERT INTO assessment_data " . $names_str;
+
+
+    for ($names as $n) {
+        $sql = $sql + $n;
+    }
+
+}
+*/
 
 // Todo: include post in purpose statement
 // purp: returns an array containing the input fields from the given number
@@ -78,32 +134,39 @@ function collectInputs($numRows, $fieldName) {
             // input is set so add it to the array
             array_push($inputArray, $_POST["$fieldName$i"]);
         }
+        else {
+            array_push($inputArray, null);
+        }
     }
     return $inputArray;
 }
 
-// purp: applies the given validation function to each item in the list
-// issuing error messages for each invalid element and returns the total number
-// of invalid elements
+// purp: returns an array where all invalid elements in the given array are null
 function validate(callable $isValid, $array) {
-    $numInvalid = 0;
+    $validArray = array();
     foreach ($array as $elem) {
-        if ($isValid($elem) == false) {
-            $numInvalid++;
+        if ($isValid($elem) == true or $elem == null) {
+            array_push($validArray, $elem);
             //echo("$elem is not valid");
         }
         else {
+            array_push($validArray, null);
             //echo("$elem is valid");
         }
     }
 }
 
+// TODO: check if this throws an error if it is of the wrong type
 // returns true if the the given string describes a valid date in one
 // of the following two formats: mm/dd/yyyy  or yyyy-dd-mm
 function isDate($str) {
-    if (validateDate($str, 'm/d/Y') or validateDate($str, 'Y-d-m')) {
+    if ($str == null) {
         return true;
-    } else {
+    }
+    elseif (validateDate($str, 'm/d/Y') or validateDate($str, 'Y-d-m')) {
+        return true;
+    }
+    else {
         return false;
     }
 }
