1379db8613479ec297cbe224f6fea52ae68bc48f
jasonkrone <jasonkrone@me.com>
2015-10-15 01:30:41 -0400

    helper method for selecting rows from table in util

--- assessment_table.util.php
+++ assessment_table.util.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jasonkrone
- * Date: 10/15/15
- * Time: 12:38 AM
- */
+
+require_once('transporter.php');
+require_once('assessment_table.config.php');
+
+//  returns an array containing every row in the
+function getAssessmentInfo() {
+    // create a query to get all rows in the table
+    // not sure how they want use to do this
+    $assessmentArray = null;
+    $transporter = new Transporter();
+    $db = $transporter->dbConnectPdo();
+    $query = "SELECT * FROM " . TABLE_NAME . ";";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    if ($rows != false) {
+        $assessmentArray = $rows;
+    }
+    return $assessmentArray;
+}
