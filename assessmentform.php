0d94655e1e171b04ac218120bc2a53cb117d91cf
David Gibbs <ddave2122@gmail.com>
2015-10-16 10:25:33 -0400

    Updated validation, input fields, and error responses

---
+++ assessmentform.php
@@ -0,0 +1,180 @@
+<?php
+//include_once("includes/topwrapper.php");
+?>
+    <script>
+        var rowCount = 1;
+        $('#loading_indicator').hide();
+        function validateForm() {
+            var recep_vocab = $("#receptive_vocab").val();
+            var letter_id = $("#letter_id").val();
+            var dec_sight = $("#dec_sight_words").val();
+            var rhyming = $("#rhyming").val();
+            var blending = $("#blending").val();
+            var nonword_rep = $("nonword_rep").val();
+
+            console.log( "Called validateForm" );
+
+            // receptive vocab max 18
+            if ( recep_vocab < 0 || recep_vocab > 18) {
+                console.log( "recep vocab score bad" );
+            }
+            // letter id ma 80
+            if ( letter_id < 0 || letter_id > 80) {
+                console.log( "letter score bad" );
+            }
+            // decodable and sight words max 11
+            if ( dec_sight < 0 || dec_sight > 11) {
+                console.log( "dec sight score bad" );
+            }
+            // rhyming max 6
+            if ( rhyming < 0 || rhyming > 6) {
+                console.log( "rhyming score bad" );
+            }
+            // blending max 6
+            if ( blending < 0 || blending > 6) {
+                console.log( "belding score bad" );
+            }
+            // non word rep max 6
+            if ( nonword_rep < 0 || nonword_rep > 6) {
+                console.log( "nonword_repition score bad" );
+            }
+        }
+
+        function submitAnimation() {
+            // indicate they can't change info
+            $('#loading_indicator').show();
+            $('#table_div').fadeTo(500,0.2);
+            $('button').each(function(){
+                this.disabled = true;
+                console.log("disabled" + this.disabled)
+            });
+
+            var data = {};
+            data["table_rows"] = rowCount;
+            $('#assessment_table input').each (function() {
+                data[this.name] = this.value;
+            });
+
+            $('#assessment_table select').each (function() {
+                data[this.name] = this.value;
+            });
+
+            $('#assessment_table textarea').each (function() {
+                data[this.name] = this.value;
+            });
+
+            $.ajax({
+                method: "POST",
+                url: "includes/ingestassessmentdata.php",
+                data: data
+            }).done(function( msg ){
+                $('.box-footer').append(msg);
+                window.setTimeout(function() { $(".alert-success").alert('close');}, 4000);
+                window.setTimeout(function() {
+                    $('#loading_indicator').hide();
+                    $('#table_div').fadeTo(500,1.0);
+                    $('button').each(function(){
+                        this.disabled = false;
+                    });
+                    // clear the table of added rows
+                    $('tbody').children('tr').not(':first').remove();
+                    // clear first row of input data
+                    $('input').val('');
+                    rowCount = 1;
+                }, 1000);
+            });
+        }
+
+        function addRow() {
+            var row = '<tr><td class=" "><input class="form-control" type="number"   id="student_id"   name="student_id" ></td> \
+                       <td class=" "><input class="form-control" type="text"   id="assessment_phase" name="assessment_phase" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="age"              name="age" ></td> \
+                       <td class=" "><select class="form-control" type="text"   id="gender"           name="gender" ><option disabled selected> -- select -- </option><option value="M">M</option><option value="F">F</option></select></td> \
+                       <td class=" "><input class="form-control" type="date"   id="testing_date"     name="testing_date" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="receptive_vocab"  name="receptive_vocab" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="letter_id_alpha"  name="letter_id_alpha" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="letter_id_rand"   name="letter_id_rand" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="sound_letter_id"  name="sound_letter_id" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="dec_sight_words"  name="dec_sight_words" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="rhyming"          name="rhyming" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="blending"         name="blending" ></td> \
+                       <td class=" "><input class="form-control" type="number" id="nonword_rep"      name="nonword_rep" required=""></td> \
+                       <td class=" "><textarea rows="1" cols="50" class="form-control" type="text"   id="comments"         name="comments"></textarea></td></tr>';
+            // taken from stack overflow
+            $('#assessment_table').find('tbody:last').append(row);
+            $('tr:last').find('.form-control').each (function() {
+                // add a suffix of the row to the name and id of each new input
+                this.id = this.id + rowCount;
+                this.name = this.name + rowCount;
+            });
+            rowCount = rowCount + 1;
+        }
+    </script>
+
+    <section class="content">
+        <div class="row">
+            <section class="col-lg-12">
+                <div class="box box-primary">
+                    <div class="box-header">
+                        <div class="box-title">
+                            Assessment Entry Form
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <div class="container-fluid">
+                            <div class="table-responsive" id="table_div">
+                                <img src="img/ajax-loader.gif" id="loading_indicator" name="loading_indicator" hidden="true">
+                                <table class="table table-bordered table-striped dataTable" id="assessment_table" name="assessment_table">
+                                    <thead>
+                                    <tr>
+                                        <th><label for="student_id0">Student ID</label></th>
+                                        <th><label for="assessment_phase0">Assesment Phase</label></th>
+                                        <th><label for="age0">Age</label></th>
+                                        <th width="80px"><label for="gender0">Gender</label></th>
+                                        <th><label for="testing_date0">Date of Testing</label></th>
+                                        <th><label for="receptive_vocab0">Receptive Vocab</label></th>
+                                        <th><label for="letter_id_alpha0">Letter Name Identification (Alphabetical Order)</label></th>
+                                        <th><label for="letter_id_rand0">Letter Name Identification (Random Order)</label></th>
+                                        <th><label for="sound_letter_id0">Sound Letter Identification</label></th>
+                                        <th><label for="dec_sight_words0">Decodable and Sight Words</label></th>
+                                        <th><label for="rhyming0">Rhyming</label></th>
+                                        <th><label for="blending0">Blending</label></th>
+                                        <th><label for="nonword_rep0">Nonword Repetition</label></th>
+                                        <th><label for="comments0">Comments</label></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td class=" "><input class="form-control" type="number" id="student_id0"       name="student_id0" ></td>
+                                        <td class=" "><input class="form-control" type="text"   id="assessment_phase0" name="assessment_phase0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="age0"              name="age0" ></td>
+                                        <td class=" "><select class="form-control" type="text"   id="gender0"          name="gender0" ><option disabled selected> -- select -- </option><option value="M">M</option><option value="F">F</option></select></td>
+                                        <td class=" "><input class="form-control" type="date"   id="testing_date0"     name="testing_date0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="receptive_vocab0"  name="receptive_vocab0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="letter_id_alpha0"  name="letter_id_alpha0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="letter_id_rand0"   name="letter_id_rand0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="sound_letter_id0"  name="sound_letter_id0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="dec_sight_words0"  name="dec_sight_words0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="rhyming0"          name="rhyming0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="blending0"         name="blending0" ></td>
+                                        <td class=" "><input class="form-control" type="number" id="nonword_rep0"      name="nonword_rep0" ></td>
+                                        <td class=" "><textarea rows="1" cols="50" class="form-control" type="text"   id="comments0"         name="comments"></textarea></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="box-footer">
+                                <button type="button" class="btn btn-info" onclick="addRow();">Add Row</button>
+                                <br />
+                                <br />
+                                <br />
+                                <button type="button" class="btn btn-primary" id="submit_button" onclick="submitAnimation();">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+
+<?php //include_once("includes/bottomwrapper.php"); ?>
--- readdata.php
+++ readdata.php
@@ -1,105 +1,155 @@
 <?php
 require_once('transporter.php');
-require_once('readdata.config.php');
-
-?>
-<div class="alert alert-success fade in" id="success_alert">
-    <a href="#" class="close" data-dismiss="alert">&times;</a>
-    <strong>Success!</strong> Your form has been sent successfully.
-</div>
-<?php
+require_once('assessmentdataconfig.php');
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-function main() {
-    $rows = $_POST[ROWS_NAME];
 
-    // collect student ids
-    $studentIDArray = collectInputs($rows, STUDENT_ID_NAME, null);
-    $studentIDArray = validate("is_numeric", $studentIDArray);
+$rows = $_POST[ROWS_NAME];
 
-    // TODO: check if this is the correct validataion
-    // collect assessment_phases
-    $assessmentPhaseArray = collectInputs($rows, ASSESSMENT_PHASE_NAME, null);
-    $assessmentPhaseArray = validate("is_string", $assessmentPhaseArray);
+// collect student ids
+$studentIDArray = collectInputs($rows, STUDENT_ID_NAME, null);
+//$studentIDArray = validate("is_numeric", $studentIDArray);
+if(!validate("is_numeric", $studentIDArray))
+    return sendErrorMessage("ID error");
 
-    // collect ages
-    $ageArray = collectInputs($rows, AGE_NAME, null);
-    $ageArray = validate("is_numeric", $ageArray);
 
-    // collect genders
-    $genderArray = collectInputs($rows, GENDER_NAME, null);
-    $genderArray = validate("is_string", $genderArray);
+// TODO: check if this is the correct validataion
+// collect assessment_phases
+$assessmentPhaseArray = collectInputs($rows, ASSESSMENT_PHASE_NAME, null);
+//$assessmentPhaseArray = validate("is_string", $assessmentPhaseArray);
+if(!validate("is_string", $assessmentPhaseArray))
+    return sendErrorMessage("Assessment phase error");
 
-    // receptive vocab score
-    $recepVocabScoreArray = collectInputs($rows, RECEPTIVE_VOCAB_NAME, 0);
-    $recepVocabScoreArray = validate("is_numeric", $recepVocabScoreArray);
 
-    // letter name id alphabetical score
-    $alphaLetterIDArray = collectInputs($rows, LETTER_ID_ALPHABETICAL_NAME, 0);
-    $alphaLetterIDArray = validate("is_numeric", $alphaLetterIDArray);
+// collect ages
+$ageArray = collectInputs($rows, AGE_NAME, null);
+//$ageArray = validate("is_numeric", $ageArray);
+if(!validate("is_numeric", $ageArray))
+    return sendErrorMessage("Age error");
 
-    // letter name id random score
-    $randomLetterIDArray = collectInputs($rows, LETTER_ID_RANDOM_NAME, 0);
-    $randomLetterIDArray = validate("is_numeric", $randomLetterIDArray);
 
-    // sound letter id
-    $soundLetterIDArray = collectInputs($rows, SOUND_LETTER_ID_NAME, 0);
-    $soundLetterIDArray = validate("is_numeric", $soundLetterIDArray);
+// collect genders
+$genderArray = collectInputs($rows, GENDER_NAME, null);
+//$genderArray = validate("is_string", $genderArray);
+if(!validate("is_string", $genderArray))
+    return sendErrorMessage("Gender error");
 
-    // dec sight word score
-    $decSightWordScoreArray = collectInputs($rows, DECODABLE_SIGHT_WORDS_NAME, 0);
-    $decSightWordScoreArray = validate("is_numeric", $decSightWordScoreArray);
 
-    // rhymiung score
-    $rhymingScoreArray = collectInputs($rows, RHYMING_NAME, 0);
-    $rhymingScoreArray = validate("is_numeric", $rhymingScoreArray);
+// receptive vocab score
+$recepVocabScoreArray = collectInputs($rows, RECEPTIVE_VOCAB_NAME, 0);
+//$recepVocabScoreArray = validate("is_numeric", $recepVocabScoreArray);
+if(!validate("is_numeric", $recepVocabScoreArray))
+    return sendErrorMessage("Receptive Vocab error");
 
-    // blending scores
-    $blendingScoreArray = collectInputs($rows, BLENDING_NAME, 0);
-    $blendingScoreArray = validate("is_numeric", $blendingScoreArray);
 
-    // nonwordRep scores
-    $nonwordRepScoreArray = collectInputs($rows, NONWORD_REPETITION_NAME, 0);
-    $nonwordRepScoreArray = validate("is_numeric", $nonwordRepScoreArray);
+// letter name id alphabetical score
+$alphaLetterIDArray = collectInputs($rows, LETTER_ID_ALPHABETICAL_NAME, 0);
+//$alphaLetterIDArray = validate("is_numeric", $alphaLetterIDArray);
+if(!validate("is_numeric", $alphaLetterIDArray))
+    return sendErrorMessage("Letter ID Alph. error");
 
-    // testing dates
-    $testingDateArray = collectInputs($rows, TESTING_DATE_NAME, null);
-    $testingDateArray = validate("isDate", $testingDateArray);
+// letter name id random score
+$randomLetterIDArray = collectInputs($rows, LETTER_ID_RANDOM_NAME, 0);
+//$randomLetterIDArray = validate("is_numeric", $randomLetterIDArray);
+if(!validate("is_numeric", $randomLetterIDArray))
+    return sendErrorMessage("Letter Id random error");
 
-    $commentArray = collectInputs($rows, COMMENTS_NAME, null);
+// sound letter id
+$soundLetterIDArray = collectInputs($rows, SOUND_LETTER_ID_NAME, 0);
+//$soundLetterIDArray = validate("is_numeric", $soundLetterIDArray);
+if(!validate("is_numeric", $soundLetterIDArray))
+    return sendErrorMessage("Sound letter error");
 
-    $tests = array($recepVocabScoreArray, $alphaLetterIDArray, $randomLetterIDArray, $soundLetterIDArray,
-                    $decSightWordScoreArray, $rhymingScoreArray, $blendingScoreArray, $nonwordRepScoreArray);
+// dec sight word score
+$decSightWordScoreArray = collectInputs($rows, DECODABLE_SIGHT_WORDS_NAME, 0);
+//$decSightWordScoreArray = validate("is_numeric", $decSightWordScoreArray);
+if(!validate("is_numeric", $decSightWordScoreArray))
+    return sendErrorMessage("Decodeable sight errori");
 
-    $totalScoreArray = getTotalScores($tests, $rows);
+// rhymiung score
+$rhymingScoreArray = collectInputs($rows, RHYMING_NAME, 0);
+//$rhymingScoreArray = validate("is_numeric", $rhymingScoreArray);
+if(!validate("is_numeric", $rhymingScoreArray))
+    return sendErrorMessage("Rhyming error");
 
-    $percentageScoreArray = getPercentageScores($totalScoreArray, $rows);
+// blending scores
+$blendingScoreArray = collectInputs($rows, BLENDING_NAME, 0);
+//$blendingScoreArray = validate("is_numeric", $blendingScoreArray);
+if(!validate("is_numeric", $blendingScoreArray))
+    return sendErrorMessage("Blending error");
 
-    // dict with field names in table and data rows for fields
-    $assessmentData = array(
-        "student_id" => $studentIDArray,
-        "assessment_phase" => $assessmentPhaseArray,
-        "age" => $ageArray,
-        "testing_date" => $testingDateArray,
-        "gender" => $genderArray,
-        "receptive_vocabulary" => $recepVocabScoreArray,
-        "letter_name_identification_in_alphabetical_order" => $alphaLetterIDArray,
-        "letter_name_identification_in_random_order" => $randomLetterIDArray,
-        "sound_letter_identification" => $soundLetterIDArray,
-        "decodeable_words_and_sight_words" => $decSightWordScoreArray,
-        "phonological_awareness_rhyming" => $rhymingScoreArray,
-        "phonological_awareness_blending" => $blendingScoreArray,
-        "phonological_awareness_non_word_repetition" => $nonwordRepScoreArray,
-        "comments" => $commentArray,
-        "total_score" => $totalScoreArray,
-        "percentage" => $percentageScoreArray,
-    );
+// nonwordRep scores
+$nonwordRepScoreArray = collectInputs($rows, NONWORD_REPETITION_NAME, 0);
+//$nonwordRepScoreArray = validate("is_numeric", $nonwordRepScoreArray);
+if(!validate("is_numeric", $nonwordRepScoreArray))
+    return sendErrorMessage("Nonword Repetition error");
 
-    insertAssessmentData($assessmentData, $rows);
-    checkInsertionSuccess($assessmentData, $rows);
+// testing dates
+$testingDateArray = collectInputs($rows, TESTING_DATE_NAME, null);
+//$testingDateArray = validate("isDate", $testingDateArray);
+if(!validate("isDate", $testingDateArray))
+    return sendErrorMessage("Testing date error");
+
+$commentArray = collectInputs($rows, COMMENTS_NAME, null);
+
+$tests = array($recepVocabScoreArray, $alphaLetterIDArray, $randomLetterIDArray, $soundLetterIDArray,
+    $decSightWordScoreArray, $rhymingScoreArray, $blendingScoreArray, $nonwordRepScoreArray);
+
+$totalScoreArray = getTotalScores($tests, $rows);
+
+$percentageScoreArray = getPercentageScores($totalScoreArray, $rows);
+
+// dict with field names in table and data rows for fields
+$assessmentData = array(
+    "student_id" => $studentIDArray,
+    "assessment_phase" => $assessmentPhaseArray,
+    "age" => $ageArray,
+    "testing_date" => $testingDateArray,
+    "gender" => $genderArray,
+    "receptive_vocabulary" => $recepVocabScoreArray,
+    "letter_name_identification_in_alphabetical_order" => $alphaLetterIDArray,
+    "letter_name_identification_in_random_order" => $randomLetterIDArray,
+    "sound_letter_identification" => $soundLetterIDArray,
+    "decodeable_words_and_sight_words" => $decSightWordScoreArray,
+    "phonological_awareness_rhyming" => $rhymingScoreArray,
+    "phonological_awareness_blending" => $blendingScoreArray,
+    "phonological_awareness_non_word_repetition" => $nonwordRepScoreArray,
+    "comments" => $commentArray,
+    "total_score" => $totalScoreArray,
+    "percentage" => $percentageScoreArray,
+);
+
+
+insertAssessmentData($assessmentData, $rows);
+if(checkInsertionSuccess($assessmentData, $rows))
+{
+    echo <<<EOT
+    <div class="alert alert-success fade in" id="success_alert">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Success!</strong> Your form has been sent successfully.
+    </div>
+EOT;
 }
+else
+{
+    sendErrorMessage();
+}
+
+
+function sendErrorMessage($msg = null)
+{
+    echo <<<EOT
+    <div class="alert alert-danger fade in" id="success_alert">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Error</strong> Unable to submit form data.  Please report the problem.<br />
+        $msg
+    </div>
+EOT;
+    return false;
+}
+
 
 
 function getPercentageScores($totalScores, $rows) {
@@ -126,7 +176,7 @@ function getTotalScores($tests, $rows) {
 
 // checks that all of our assessmentData was inserted into mysql
 function checkInsertionSuccess($data, $rows) {
-    $success = true;
+    $success = false;
     $count = count($data);
     $transporter = new Transporter();
     $db = $transporter->dbConnectPdo();
@@ -152,8 +202,8 @@ function checkInsertionSuccess($data, $rows) {
         $statement = $db->prepare($sql);
         $statement->execute();
         $row = $statement->fetch(PDO::FETCH_ASSOC);
-        if ($row == false) {
-            $success = false;
+        if ($row == true) {
+            $success = true;
         }
     }
     $db = null;
@@ -177,16 +227,21 @@ function insertAssessmentData($field_data_dict, $numRows) {
         }
         $valsStr = queryStr($values, "format_value");
         // form the first part of the query
-        $sql = "INSERT INTO " . TABLE_NAME . " " . $fieldStr . " VALUES " . $valsStr . ";";
+        $sql = "INSERT INTO assessment_information " . $fieldStr . " VALUES " . $valsStr . ";";
+        var_dump($sql);
+        print_r($sql);
+        die();
         $statement = $db->prepare($sql);
         $result = $statement->execute();
 
         if ($result == false) {
-            print_r($db->errorInfo());
+            sendErrorMessage();
+//            print_r($db->errorInfo());
         }
     }
     // make space
-    $transporter = null;
+    unset($transporter);
+//    $transporter = null;
 }
 
 
@@ -249,19 +304,29 @@ function collectInputs($numRows, $fieldName, $default) {
 
 
 // returns an array where all invalid elements in the given array are null
-function validate(callable $isValid, $array) {
-    $validArray = array();
+//function validate(callable $isValid, $array) {
+//    $validArray = array();
+//    foreach ($array as $elem) {
+//        if ($isValid($elem) == true or $elem == null) {
+//            array_push($validArray, $elem);
+//        }
+//        else {
+//            array_push($validArray, null);
+//        }
+//    }
+//    return $validArray;
+//}
+
+//Check if the array is valid
+function validate(callable $isValid, $array)
+{
     foreach ($array as $elem) {
-        if ($isValid($elem) == true or $elem == null) {
-            array_push($validArray, $elem);
-        }
-        else {
-            array_push($validArray, null);
+        if (!$isValid($elem) && strlen($elem) <= 0) {
+            return false;
         }
     }
-    return $validArray;
+    return true;
 }
-
 
 // returns true if the the given string describes a valid date in one
 // of the following two formats: mm/dd/yyyy  or yyyy-dd-mm
@@ -279,9 +344,3 @@ function validateDate($date, $format = 'm-d-Y') {
     $d = DateTime::createFromFormat($format, $date);
     return $d;
 }
-
-
-main();
-
-
-
