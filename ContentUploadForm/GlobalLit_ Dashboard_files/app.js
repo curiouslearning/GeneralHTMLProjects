5c1b0b5356ef93708d21b07a1b2244f9185c0fb7
Kate Harwood <kateharwood>
2015-12-09 18:19:47 -0500

    updated checkboxtree

--- ContentUploadForm/GlobalLit_ Dashboard_files/app.js
+++ ContentUploadForm/GlobalLit_ Dashboard_files/app.js
@@ -153,7 +153,7 @@ $(function() {
      * iCheck plugin in.
      * You can find the documentation at http://fronteed.com/iCheck/
      */
-    $("input[type='checkbox']:not(.simple), input[type='radio']:not(.simple)").iCheck({
+    $(/*"input[type='checkbox']:not(.simple),*/ "input[type='radio']:not(.simple)").iCheck({
         checkboxClass: 'icheckbox_minimal',
         radioClass: 'iradio_minimal'
     });
